1df01a5f35561bafce68e8cef431d37263b7f044
Dawei.H <hoodavy@gmail.com>
2020-02-05 17:35:40 +0800

    Sample HTML content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -15,5 +16,12 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
-    println!("Request:\n{}", String::from_utf8_lossy(&buffer[..]));
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    // println!("Request:\n{}", String::from_utf8_lossy(&buffer[..]));
 }
